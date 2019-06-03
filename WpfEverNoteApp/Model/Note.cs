bf0234acded576360485bc3ac2262514084010cc
Rafał Nowakowski <gee@vp.pl>
2019-06-03 14:51:42 +0200

    Initialize app, add model with PropertyChanged.Fody

---
+++ WpfEverNoteApp/Model/Note.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace WpfEverNoteApp.Model
+{
+    class Note : INotifyPropertyChanged
+    {
+        public int Id { get; set; }
+        public int NotebookId { get; set; }
+        public string Title { get; set; }
+        public DateTime CreatedTime { get; set; }
+        public DateTime UpdatedTime { get; set; }
+        public string FileLocation { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
---
+++ WpfEverNoteApp/Model/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfEverNoteApp.Model
+{
+    class User : INotifyPropertyChanged
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Username { get; set; }
+        public string Lastname { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
